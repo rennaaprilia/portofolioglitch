7c30b62ae332be2ccc4a52d644369fa7b099bd87
Renna Aprilia <rennaaprilia@student.uns.ac.id>
2023-10-14 02:58:22 +0700

    freeze dotenv and other requirements

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
