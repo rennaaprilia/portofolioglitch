a9209294d6ba1fc7d74f6b9cd3c778ffebfa8a5c
Renna Aprilia <92446979+rennaaprilia@users.noreply.github.com>
2023-10-14 10:45:46 +0700

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
 from flask import Flask, render_template, request, jsonify
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
